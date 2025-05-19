105a672b7585e0756a450148908aab10b34aaf37
Beatriz Batista <byah24@outlook.com>
2025-05-19 08:55:38 -0300

    Anotações Finalizadas

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// string resposta = "";
+
+// int contador = 1;
+
+// while (contador <= 10)
+// {
+//     Console.Write($"{contador} ");
+//     contador += 1;
+// }
+
+
+while (true)
+{
+    Console.WriteLine("Laço Infinito");
+}
